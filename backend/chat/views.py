15920e9e4d25aa700332142c5f3d78ca8390551d
Rustam Yunusov <yrustt@yandex.ru>
2020-12-06 11:16:38 +0300

    Added serializer for chats

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import Chat, Message
+from .serializers import ChatSerializer
+
+
+class ChatViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Chat.objects.all()
+        serializer = ChatSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        chat = get_object_or_404(Chat, pk=pk)
+        serializer = ChatSerializer(chat)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ChatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        chat = get_object_or_404(Chat, pk=pk)
+        chat.delete()
+        return Response(status=status.HTTP_200_OK)
